0f43874dd727ae665a659a21ee6088d33a2e226a
Dongho <backack2@gmail.com>
2015-11-30 16:51:30 +0900

    자랑하기 리스트 컨트롤러 분리

--- client/app/envy/list/list.controller.js
+++ client/app/envy/list/list.controller.js
@@ -1,15 +1,44 @@
 'use strict';
 
 angular.module('ambestApp')
-  .controller('EnvyListCtrl', function ($scope, Envys) {
-    $scope.envyInfo = null
+  .controller('EnvyListCtrl', function ($scope, Auth, $location) {
+    $scope.isCollapsed = true
+    $scope.isLoggedIn = Auth.isLoggedIn
+    $scope.isAdmin = Auth.isAdmin
+    $scope.getCurrentUser = Auth.getCurrentUser
+    $scope.selectedIndex = 0
+    // account
+    console.log('envyCtrl')
 
-    function all() {
-    	Envys.query(function (envys) {
-    		$scope.envyInfo = envys
-    	}, function (err) {
-    		console.error(err)
-    	})
+    $scope.logout = function() {
+      Auth.logout()
+      // $scope.$emit('div_close')
+      $location.path('/logout')
+    }
+    $scope.signup = function() {
+      // $mdSidenav('right').close()
+      $location.path('/signup')
+    }
+    $scope.login = function() {
+      // $mdSidenav('right').close()
+      $location.path('/login')
     }
 
-  });
+    
+  })
+  .controller('EnvyListFreshCtrl', function ($scope, Envys, $location) {
+    console.log('freshCTRL')
+    $scope.envyInfo = null
+    function all() {
+      Envys.query(function (envys) {
+        console.log(envys)
+        $scope.envyInfo = envys
+      }, function (err) {
+        console.error(err)
+      })
+    }
+    $scope.envyInfo = all()
+    $scope.go = function (id) {
+      $location.path('/envy/view/'+id)
+    }
+  })
